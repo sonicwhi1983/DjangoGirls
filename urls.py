513b5320fdb66fd853a7cbef45f65ea90811988a
sonicwhi1983 <98901866+sonicwhi1983@users.noreply.github.com>
2024-12-03 12:40:47 -0500

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,12 @@
+from django.urls import path 
+from . import views 
+
+
+
+# a list that lists all the urls that we will make point to html pages/views
+# it is contained in a list called urlpatterns
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
+  
